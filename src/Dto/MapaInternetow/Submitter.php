8b461b524a80cf38a8aa58dd239a033b0094d892
cvgore <hi@cvgo.re>
2024-12-13 11:57:42 +0100

    mapa internetow quick fix

--- src/Dto/MapaInternetow/Submitter.php
+++ src/Dto/MapaInternetow/Submitter.php
@@ -7,7 +7,7 @@ namespace Cvgore\RandomThings\Dto\MapaInternetow;
 final class Submitter
 {
 	public function __construct(
-		public string $type,
+		public ?string $type,
 		public ?string $user,
 	) {
 	}
